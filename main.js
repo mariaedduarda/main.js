021e0e6158e9e0f9b7896a4a84e03a1869009b74
mariaedduarda <119062239+mariaedduarda@users.noreply.github.com>
2024-04-01 12:13:24 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,35 @@
+const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
+
+for(let i=0;i<botoes.length;i++){
+    botoes[i].onclick = function() {
+        for(let j=0; j<botoes.length;j++){
+            botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
+        }
+        botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo");
+    }
+}
+
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date ("2023-10-05T00:00:00");
+const tempoObjetivo2 = new Date ("2023-12-05T00:00:00");
+const tempoObjetivo3 = new Date ("2023-12-30T00:00:00");
+const tempoObjetivo4 = new Date ("2024-02-01T00:00:00");
+
+const tempo =[tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
+
+function calculatempo(tempoObjetivo){
+    let tempoAtual = newDate();
+    let tempoFinal = tempoObjetivo - tempoAtual
+    let  segundos = math.floor (tempoFinal/1000);
+    let  minutos = math.floor (segundos/60);
+    let horas = math.floor (minutos/60);
+    let dias = math.floor (horas/24);
+    segundos %=60;
+    minutos %=60;
+    horas %=24;
+}
+
+ 
